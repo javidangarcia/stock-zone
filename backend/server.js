d59101986c312a98507061836b7ef0e147c6257a
Javier Garcia <javierdangarcia@gmail.com>
2023-07-24 14:06:27 -0700

    added frontend url env variable for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(express.json());
