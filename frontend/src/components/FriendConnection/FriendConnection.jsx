3aed91f22e3513e5ddae22b87e36265680812866
Javier Garcia <javierdangarcia@gmail.com>
2023-07-27 09:02:56 -0700

    redesigned profile and profile history components

--- frontend/src/components/FriendConnection/FriendConnection.jsx
+++ frontend/src/components/FriendConnection/FriendConnection.jsx
@@ -2,6 +2,7 @@ import "./FriendConnection.css";
 import axios from "axios";
 import { useContext } from "react";
 import { UserContext } from "../App/App";
+import Button from "react-bootstrap/Button";
 
 export default function FriendConnection({ username, profile, setProfile }) {
     const { user, setErrorMessage } = useContext(UserContext);
@@ -14,7 +15,7 @@ export default function FriendConnection({ username, profile, setProfile }) {
             const response = await axios.post(
                 url,
                 { username },
-                { withCredentials: true, validateStatus: () => true } 
+                { withCredentials: true, validateStatus: () => true }
             );
 
             if (response.status === 200) {
@@ -33,13 +34,13 @@ export default function FriendConnection({ username, profile, setProfile }) {
         } catch (error) {
             setErrorMessage(`${error.message}: Please try again later.`);
         }
-    }
+    };
 
     return (
-        <div className={username === user.username ? "hidden" : "friend"}>
-            <button onClick={handleFriend}>
+        <div className={username === user.username ? "hidden" : ""}>
+            <Button className="friend-button" variant="primary" onClick={handleFriend}>
                 {profile.friend ? "Remove Friend" : "Add Friend"}
-            </button>
+            </Button>
         </div>
-    )
+    );
 }
--- frontend/src/components/ProfileHistory/ProfileHistory.jsx
+++ frontend/src/components/ProfileHistory/ProfileHistory.jsx
@@ -3,21 +3,49 @@ import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { UserContext } from "../App/App";
 import { Link } from "react-router-dom";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import Card from "react-bootstrap/Card";
 
 export default function ProfileHistory({ username }) {
     const [followsHistory, setFollowsHistory] = useState([]);
     const [likesHistory, setLikesHistory] = useState([]);
     const [dislikesHistory, setDislikesHistory] = useState([]);
-    const { user, setErrorMessage } = useContext(UserContext);
+    const { setErrorMessage } = useContext(UserContext);
+    const [mode, setMode] = useState([]);
 
     useEffect(() => {
         const fetchHistory = async () => {
             try {
                 const [followsResponse, likesResponse, dislikesResponse] =
                     await Promise.all([
-                        axios.get(`${import.meta.env.VITE_HOST}/follows/user/${username}`, { withCredentials: true, validateStatus: () => true }),
-                        axios.get(`${import.meta.env.VITE_HOST}/likes/user/${username}`, { withCredentials: true, validateStatus: () => true }),
-                        axios.get(`${import.meta.env.VITE_HOST}/dislikes/user/${username}`, { withCredentials: true, validateStatus: () => true })
+                        axios.get(
+                            `${
+                                import.meta.env.VITE_HOST
+                            }/follows/user/${username}`,
+                            {
+                                withCredentials: true,
+                                validateStatus: () => true
+                            }
+                        ),
+                        axios.get(
+                            `${
+                                import.meta.env.VITE_HOST
+                            }/likes/user/${username}`,
+                            {
+                                withCredentials: true,
+                                validateStatus: () => true
+                            }
+                        ),
+                        axios.get(
+                            `${
+                                import.meta.env.VITE_HOST
+                            }/dislikes/user/${username}`,
+                            {
+                                withCredentials: true,
+                                validateStatus: () => true
+                            }
+                        )
                     ]);
 
                 if (followsResponse.status === 200) {
@@ -25,9 +53,11 @@ export default function ProfileHistory({ username }) {
                     setLikesHistory(likesResponse.data.stocksYouLike);
                     setDislikesHistory(dislikesResponse.data.stocksYouDislike);
                 }
-    
+
                 if (followsResponse.status === 500) {
-                    setErrorMessage(`${response.statusText}: Please try again later.`);
+                    setErrorMessage(
+                        `${response.statusText}: Please try again later.`
+                    );
                 }
             } catch (error) {
                 setErrorMessage(`${error.message}: Please try again later.`);
@@ -37,58 +67,61 @@ export default function ProfileHistory({ username }) {
     }, [username]);
 
     return (
-        <div className="profile-history">
-            <div className="follows-history">
-                <p>{username === user.username ? "Stocks You Follow" : "Stocks They Follow"}</p>
-                {
-                    followsHistory?.map((stock) => {
-                        return (
-                            <div key={stock.ticker} className="stock-margin">
-                                <Link key={stock.name} to={`/search/stocks/${stock.ticker}`} className="stock-link">
-                                    <div className="stock">
-                                        <img src={stock.logo} alt={`This is a logo of ${stock.name}.`} />
-                                        <p>{stock.ticker}</p>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
+        <>
+            <ToggleButtonGroup
+                className="mt-5"
+                type="radio"
+                name="options"
+                defaultValue={1}
+            >
+                <div className="ms-2">
+                    <ToggleButton
+                        id="tbg-radio-1"
+                        value={1}
+                        variant="primary"
+                        onClick={() => setMode(followsHistory)}
+                    >
+                        Follows
+                    </ToggleButton>
+                </div>
+                <div className="me-2 ms-2">
+                    <ToggleButton
+                        id="tbg-radio-2"
+                        value={2}
+                        variant="success"
+                        onClick={() => setMode(likesHistory)}
+                    >
+                        Likes
+                    </ToggleButton>
+                </div>
+                <div className="me-2">
+                    <ToggleButton
+                        id="tbg-radio-3"
+                        value={3}
+                        variant="danger"
+                        onClick={() => setMode(dislikesHistory)}
+                    >
+                        Dislikes
+                    </ToggleButton>
+                </div>
+            </ToggleButtonGroup>
+            <div className="container mt-5 mb-5">
+                <div className="row row-cols-1 row-cols-md-5 g-4">
+                    {mode?.map((stock) => (
+                        <div key={stock.ticker} className="col">
+                            <Link to={`/search/stocks/${stock.ticker}`} className="stock-link">
+                                <Card>
+                                    <Card.Img variant="top" src={stock.logo} />
+                                    <Card.Body>
+                                        <Card.Title>{stock.ticker}</Card.Title>
+                                        <Card.Text>{stock.name}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className="likes-history">
-                <p>{username === user.username ? "Stocks You Like" : "Stocks They Like"}</p>
-                {
-                    likesHistory?.map((stock) => {
-                        return (
-                            <div key={stock.ticker} className="stock-margin">
-                                <Link key={stock.name} to={`/search/stocks/${stock.ticker}`} className="stock-link">
-                                    <div className="stock">
-                                        <img src={stock.logo} alt={`This is a logo of ${stock.name}.`} />
-                                        <p>{stock.ticker}</p>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-            <div className="dislikes-history">
-                <p>{username === user.username ? "Stocks You Dislike" : "Stocks They Dislike"}</p>
-                {
-                    dislikesHistory?.map((stock) => {
-                        return (
-                            <div key={stock.ticker} className="stock-margin">
-                                <Link key={stock.name} to={`/search/stocks/${stock.ticker}`} className="stock-link">
-                                    <div className="stock">
-                                        <img src={stock.logo} alt={`This is a logo of ${stock.name}.`} />
-                                        <p>{stock.ticker}</p>
-                                    </div>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+        </>
+    );
 }
