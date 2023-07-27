4e0cb9487cad3774f543d010f0bd49a96403d9a8
Javier Garcia <javierdangarcia@gmail.com>
2023-07-27 14:20:08 -0700

    redesigned ranking and added load more popular stocks

--- frontend/src/components/Ranking/Ranking.jsx
+++ frontend/src/components/Ranking/Ranking.jsx
@@ -3,10 +3,15 @@ import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { UserContext } from "../App/App";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const STOCKS_TO_SHOW = 10;
 
 export default function Ranking() {
     const [stocksRanking, setStocksRanking] = useState([]);
     const { setErrorMessage, setLoading } = useContext(UserContext);
+    const [stocksToShow, setStocksToShow] = useState(STOCKS_TO_SHOW);
 
     useEffect(() => {
         const fetchRanking = async () => {
@@ -40,37 +45,48 @@ export default function Ranking() {
     }, []);
 
     return (
-        <div className="ranking">
-            <h1>Popular Stocks</h1>
-            <p>Recommended based on your profile and history.</p>
-            <div className="stock-list">
-                {stocksRanking?.map((stock, index) => {
-                    return (
-                        <div key={stock.ticker} className="stock-margin">
-                            <Link
-                                key={stock.name}
-                                to={`/search/stocks/${stock.ticker}`}
-                                className="stock-link"
-                            >
-                                <div className="stock">
-                                    <div className="stock-rank">
-                                        <p>{index + 1}</p>
-                                    </div>
-                                    <img
-                                        src={stock.logo}
-                                        alt={`This is a logo of ${stock.name}.`}
-                                    />
-                                    <div className="stock-info">
-                                        <p>
-                                            {stock.name} ({stock.ticker})
-                                        </p>
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
-                    );
-                })}
-            </div>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+            <h1 className="mt-4">Popular Stocks</h1>
+            <p className="mb-4">
+                Recommended based on your profile and history.
+            </p>
+            {stocksRanking?.slice(0, stocksToShow).map((stock, index) => (
+                <div className="mb-4">
+                    <Link
+                        to={`/search/stocks/${stock.ticker}`}
+                        className="stock-link"
+                    >
+                        <Card key={stock.id} className="stock-rank-card">
+                            <div>
+                                <Card.Img variant="top" src={stock.logo} />
+                                <p
+                                    className={`stock-rank fs-3 ${
+                                        index === 0 ? "text-primary" : ""
+                                    }`}
+                                >
+                                    {index + 1}
+                                </p>
+                            </div>
+                            <Card.Body>
+                                <Card.Text className="text-center">
+                                    {stock.name} ({stock.ticker})
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Link>
+                </div>
+            ))}
+            {stocksRanking?.length > stocksToShow ? (
+                <Button
+                    className="mb-5 mt-4"
+                    variant="outline-primary"
+                    onClick={() =>
+                        setStocksToShow((prev) => prev + STOCKS_TO_SHOW)
+                    }
+                >
+                    Load More
+                </Button>
+            ) : null}
         </div>
     );
 }
