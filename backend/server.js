8e5d8c37dd3c67cda8b364acaddd3e1bfe0c6151
Javier Garcia <javierdangarcia@gmail.com>
2023-07-28 09:57:52 -0700

    fixed cookie session props

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,8 @@ app.use(
         saveUninitialized: false,
         store: sessionStore,
         cookie: {
-            sameSite: false,
-            secure: false,
+            sameSite: "none",
+            secure: true,
             expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) // 1 year in milliseconds
         }
     })
